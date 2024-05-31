faed0f004ed9fb123618d78dee428b8edc521417
Kira McLean <email@kiramclean.com>
2024-05-31 12:37:13 -0300

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ const nextConfig = {
             source: "/api/test2",
             destination:
                 process.env.NODE_ENV === "development"
-                ? "http://127.0.0.1:8000/api/v2/competition/evaluate"
-                : "/api/v2/competition/evaluate"          }
+                ? "http://localhost:8000/api/test2"
+                : "/api/test2"          }
         ]
       }
 };
